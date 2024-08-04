c6ad7d1180b30ba9e2e0983c7af6e36e68c1894a
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-08-04 12:43:46 -0700

    added valid anagram solution

--- 1. Arrays & Hashing/Valid Anagram.ts
+++ 1. Arrays & Hashing/Valid Anagram.ts
@@ -15,3 +15,22 @@ Output: false
 
 */
 
+// 284 ms (horrible)
+
+function isAnagram(s: string, t: string): boolean {
+    let newT: string = t;
+    let found:boolean = false;
+    if (s.length !== t.length) return false;
+    for (let i: number = 0; i < s.length; i++) {
+        for (let j: number = 0; j < newT.length; j++) {
+            found = false;
+            if (s[i] === newT[j]) {
+                newT = newT.slice(0, j).concat(newT.slice(j + 1, newT.length));
+                found = true;
+                break;
+            }
+        }
+        if (found === false) return false;
+    }
+    return (newT.length === 0);
+};
