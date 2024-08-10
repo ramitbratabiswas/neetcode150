05b5c25079da10cc148369321b98c1970d49f970
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-08-10 15:22:42 -0700

    added trapping rain water description

--- 2. Two Pointers/Trapping Rain Water.ts
+++ 2. Two Pointers/Trapping Rain Water.ts
@@ -1,4 +1,9 @@
+/*
+  Given n non-negative integers representing an elevation map
+  where the width of each bar is 1, compute how much water it can trap after raining.
+*/
 
+// 46.41%, 58.88%
 
 function trap(height: number[]): number {
   if (height.length === 1) return 0;
