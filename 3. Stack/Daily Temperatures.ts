095d271014e191814a620ad6d9428e7785e71f19
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-08-10 18:50:26 -0700

    added daily temp description

--- 3. Stack/Daily Temperatures.ts
+++ 3. Stack/Daily Temperatures.ts
@@ -1,3 +1,12 @@
+/*
+  Given an array of integers temperatures represents the daily temperatures,
+  return an array answer such that answer[i] is the number of days you have to wait
+  after the ith day to get a warmer temperature. If there is no future day for which
+  this is possible, keep answer[i] == 0 instead.
+*/
+
+// 15.91%, 5.59%
+
 function dailyTemperatures(temperatures: number[]): number[] {
   let result: number[] = Array(temperatures.length).fill(0);
   let stack: [number, number][] = [];
