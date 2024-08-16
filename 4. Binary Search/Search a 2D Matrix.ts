1d1b911cf22fed5bc71928b17626da832c61aac0
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-08-16 14:28:08 -0700

    added searchMatrix description

--- 4. Binary Search/Search a 2D Matrix.ts
+++ 4. Binary Search/Search a 2D Matrix.ts
@@ -1,4 +1,14 @@
+/*
+  You are given an m x n integer matrix matrix with the following two properties:
 
+  Each row is sorted in non-decreasing order.
+  The first integer of each row is greater than the last integer of the previous row.
+  Given an integer target, return true if target is in matrix or false otherwise.
+
+  You must write a solution in O(log(m * n)) time complexity.
+*/
+
+// 96.58%, 18.96%
 
 function searchMatrix(matrix: number[][], target: number): boolean {
   let m = matrix.length, n = matrix[0].length;
